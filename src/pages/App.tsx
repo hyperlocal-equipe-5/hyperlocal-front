91548dd2fad920093dba5081550cb135393a299e
Pedro Lopes <lopesphls@gmail.com>
2023-03-01 21:48:07 -0300

    chore: add config tailwindcss #1

---
+++ src/pages/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="flex flex-col w-full h-screen bg-[#010000] text-[#fefbff] items-center justify-center">
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<h1>Vite + React</h1>
+				<div className="card">
+					<button
+						onClick={() => {
+							setCount(count => count + 1);
+						}}>
+						count is {count}
+					</button>
+					<p>
+						Edit <code>src/App.tsx</code> and save to test HMR
+					</p>
+				</div>
+				<p className="read-the-docs">
+					Click on the Vite and React logos to learn more
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default App;
