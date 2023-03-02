d35bf078b71089e91599804849b641f6f7e7f10e
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-02 13:38:40 -0300

    feat: create navigate pages #2

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,29 +1,22 @@
 /* eslint-disable prettier/prettier */
-import { useState } from 'react';
+
+import { Outlet } from "react-router";
+import NavBar from "./components/NavBar";
+
 
 const App = () => {
-	const [count, setCount] = useState(0);
+	
 
 	return (
-		<div className="flex flex-col w-full h-screen bg-[#010000] text-[#fefbff] items-center justify-center">
+		<div className="flex items-center justify-center">
 			<div>
-				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<h1>Vite + React</h1>
-				<div className="card">
-					<button
-						onClick={() => {
-							setCount(count => count + 1);
-						}}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
-				</div>
+				
+				<h1>header</h1>
+				<NavBar/>
+			    <Outlet/>
+				
 				<p className="read-the-docs">
-					Click on the Vite and React logos to learn more
+					foter
 				</p>
 			</div>
 		</div>
---
+++ src/pages/cadastro/Cadastro.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+function Cadastro() {
+  return (
+		<div className="flex flex-col w-full h-screen bg-[#010000] text-[#fefbff] items-center justify-center">
+			Cadastro
+		</div>
+	);
+}
+
+export default Cadastro
