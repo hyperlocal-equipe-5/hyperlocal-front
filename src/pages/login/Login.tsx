e3de39fa53785387c6dd64781ddb095ba9cb0222
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-03 09:50:23 -0300

    feat: update pages login #2

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,21 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React from 'react';
 
 function Login() {
-  return (
-    <div className="flex flex-col w-full h-screen bg-[#010000] text-[#fefbff] items-center justify-center">
-        Login</div>
-  )
+	return (
+		<body className=" m-0 bg-gray-600 ">
+			<section className=" bg-red-700 border-spacing-12 caret-red-800" >
+				<div className="  flex-col relative ">
+					<h1>Login</h1>
+					<div className=" bg-slate-600 ">
+						<input type="text" placeholder="Digite seu nome" />
+						<input type="text" placeholder="Digite seu nome" />
+						<input type="text" placeholder="Digite seu nome" />
+					</div>
+				</div>
+			</section>
+		</body>
+	);
 }
 
-export default Login
+export default Login;
