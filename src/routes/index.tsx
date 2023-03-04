e5c6f31e1d9b9d5086a6ad89edabd579755c7245
Pedro Lopes <lopesphls@gmail.com>
2023-03-04 18:52:33 -0300

    fix: fixing route error #15

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,11 @@
-import {
-	Route,
-	createBrowserRouter,
-	createRoutesFromElements,
-} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 
-const Routes = createBrowserRouter(
-	createRoutesFromElements(
-		<Route index element={<Home />} />,
-		// <Route path='/form' element={<Home />} />
-		// <Route path='/user' element={<Home />} />
-		// <Route path='/login' element={<Home />} />
-		// <Route path='*' element={<404 />} />
-	),
-);
+const Routes = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+]);
 
 export default Routes;
