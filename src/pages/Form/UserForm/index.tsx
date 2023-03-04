340c0d3935e2636318964e834bb9594d444c709a
Pedro Lopes <lopesphls@gmail.com>
2023-03-03 21:27:34 -0300

    feat: create page form and user form #4

---
+++ src/pages/Form/UserForm/index.tsx
@@ -0,0 +1,12 @@
+import styled from './styled.module.scss';
+
+const UserForm = () => {
+	return (
+		<div className={styled.test}>
+			<h1 className={styled.teste3}>teste</h1>
+			<h1 className="teste3">teste 2</h1>
+			<h1 className="teste3">teste 3 </h1>
+		</div>
+	);
+};
+export default UserForm;
---
+++ src/pages/Form/index.tsx
@@ -0,0 +1,12 @@
+import UserForm from './UserForm';
+import styled from './styled.module.scss';
+const Form = () => {
+	return (
+		<div className={styled.test}>
+			<UserForm />
+			<h4 className={styled.teste3}>Tiago</h4>
+		</div>
+	);
+};
+
+export default Form;
