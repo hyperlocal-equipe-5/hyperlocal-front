6f56dc2b37eca5b9ad5f5c100976615333c3c468
Pedro Lopes <lopesphls@gmail.com>
2023-03-04 09:51:29 -0300

    fix: add reset css #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './index.scss';
+import './reset.css';
 import Routes from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
