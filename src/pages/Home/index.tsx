d75b793743ce9038c84758ff33607eb3b4ce7654
Pedro Lopes <lopesphls@gmail.com>
2023-03-10 22:34:52 -0300

    fix: fixing merge errors and creating example CategoryBox

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,48 @@
+import CategoryBox from '../../components/CategoryBox';
 import { Container } from '../Form/styled';
 
-// const teste = [
-// 	{
-// 		LinkProduct: 'restaurante1',
-// 		image: 'restaurante 2',
-// 		NameProduct: 'restaurante 3',
-// 	},
-// 	{
-// 		LinkProduct: 'restaurante2',
-// 		image: 'restaurante 2',
-// 		NameProduct: 'restaurante 3',
-// 	},
-// 	{
-// 		LinkProduct: 'restaurante3',
-// 		image: 'restaurante 2',
-// 		NameProduct: 'restaurante 3',
-// 	},
-// 	{
-// 		LinkProduct: 'restaurante4',
-// 		image: 'restaurante 2',
-// 		NameProduct: 'restaurante 3',
-// 	},
-// 	{
-// 		LinkProduct: 'restaurante',
-// 		image: 'restaurante 2',
-// 		NameProduct: 'restaurante 3',
-// 	},
-// 	{
-// 		LinkProduct: 'restaurante1',
-// 		image: 'restaurante 1',
-// 		NameProduct: 'restaurante 4',
-// 	},
-// ];
+const teste = [
+	{
+		LinkProduct: 'restaurante1',
+		image: 'restaurante 2',
+		NameProduct: 'restaurante 3',
+	},
+	{
+		LinkProduct: 'restaurante2',
+		image: 'restaurante 2',
+		NameProduct: 'restaurante 3',
+	},
+	{
+		LinkProduct: 'restaurante3',
+		image: 'restaurante 2',
+		NameProduct: 'restaurante 3',
+	},
+	{
+		LinkProduct: 'restaurante4',
+		image: 'restaurante 2',
+		NameProduct: 'restaurante 3',
+	},
+	{
+		LinkProduct: 'restaurante',
+		image: 'restaurante 2',
+		NameProduct: 'restaurante 3',
+	},
+	{
+		LinkProduct: 'restaurante1',
+		image: 'restaurante 1',
+		NameProduct: 'restaurante 4',
+	},
+];
 // const teste2 = ['Funcionários', 'Funcionários', 'Funcionários 3'];
 const Home = () => {
-	return <Container></Container>;
+	return (
+		<Container>
+			<CategoryBox LinkCategory="teste" NameCategory="Titulo" Product={teste} />
+			<CategoryBox LinkCategory="teste" NameCategory="Titulo" Product={teste} />
+			<CategoryBox LinkCategory="teste" NameCategory="Titulo" Product={teste} />
+			<CategoryBox LinkCategory="teste" NameCategory="Titulo" Product={teste} />
+		</Container>
+	);
 };
 
 export default Home;
