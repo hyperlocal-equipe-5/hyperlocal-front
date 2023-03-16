4413eab6e3c8658c5c76ea18d5cd28b717c0ceb6
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-16 19:57:48 -0300

    freat: generate qrcode

--- src/pages/Qrcode/Qrcode.tsx
+++ src/pages/Qrcode/Qrcode.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-undef */
+import React, { useState } from 'react';
+import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
+import './Qrcode.scss';
 
 const Qrcode = () => {
-	return <div>Qrcode</div>;
+	const [qrlink, setQrlink] = useState('');
+	const [qrcode, setQrcode] = useState('');
+
+	function handrleGenerate(linkUrl: string) {
+		QRCodeLink.toDataURL(
+			linkUrl,
+			{
+				width: 600,
+				margin: 3,
+			},
+			function (_err, url) {
+				setQrcode(url);
+			},
+		);
+	}
+
+	function handleQrcode(e: any) {
+		setQrlink(e.target.value);
+		handrleGenerate(e.target.value);
+	}
+
+	return (
+		<div className="container">
+			<QRCode value={qrlink} />
+			<input
+				type="input"
+				placeholder="Digite o Qrcode desejado"
+				value={qrlink}
+				onChange={e => {
+					handleQrcode(e);
+				}}
+			/>
+			<a href={qrcode} download={`qrcode.png`}>
+				{' '}
+				Baixar Qrcode{' '}
+			</a>
+		</div>
+	);
 };
 
 export default Qrcode;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,6 +2,7 @@ import { Fragment } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Cadastro from '../pages/Cadastro/Cadastro';
 import Login from '../pages/Login/Login';
+import Qrcode from '../pages/Qrcode/Qrcode';
 
 const RoutesApp = () => {
 	return (
@@ -11,6 +12,7 @@ const RoutesApp = () => {
 					<Route path="/" element={<Login />} />
 					<Route path="/cadastro" element={<Cadastro />} />
 					<Route path="*" element={<Login />} />
+					<Route path="/qrcode" element={<Qrcode />} />
 				</Routes>
 			</Fragment>
 		</BrowserRouter>
