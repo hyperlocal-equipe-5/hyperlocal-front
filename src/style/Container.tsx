bed3261af6e2d00262c5447da831289a937f07c6
Pedro Lopes <lopesphls@gmail.com>
2023-03-23 23:13:52 -0300

    fix: resolving building problem

--- src/style/Container.tsx
+++ src/style/Container.tsx
@@ -1,10 +1,12 @@
 interface ContainerProps {
 	children: React.ReactNode;
+	styleInline?: string;
 }
 
-const Container = ({ children }: ContainerProps) => {
+const Container = ({ children, styleInline }: ContainerProps) => {
 	return (
-		<div className="flex flex-col items-center justify-between w-full min-h-screen max-h-full py-16 mobile:overflow-x-hidden bg-bg">
+		<div
+			className={`flex flex-col items-center justify-between w-full min-h-screen max-h-full py-16 mobile:overflow-x-hidden bg-bg ${styleInline}`}>
 			{children}
 		</div>
 	);
--- src/style/Help.tsx
+++ src/style/Help.tsx
@@ -1,15 +1,16 @@
 import { IoIosHelpCircleOutline } from 'react-icons/io';
 
 interface HelpProps {
-	children: React.ReactNode;
+	children?: React.ReactNode;
+	Text: string | undefined;
 }
 
-const Help = ({ children }: HelpProps) => {
+const Help = ({ children, Text }: HelpProps) => {
 	return (
 		<>
 			<div
 				className={`flex flex-row items-center text-textColor text-2xl pl-1`}
-				title={children}>
+				title={Text}>
 				<IoIosHelpCircleOutline className="text-textColor" />
 			</div>
 		</>
