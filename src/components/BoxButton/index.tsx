06d7943ed1382a4719e882e64eded4595bd06486
Pedro Lopes <lopesphls@gmail.com>
2023-03-29 11:55:02 -0300

    change: changing name categoryBox for RowBox

--- src/components/BoxButton/index.tsx
+++ src/components/BoxButton/index.tsx
@@ -10,14 +10,14 @@ const BoxButton = ({ img, title, click }: IBoxButton) => {
 			<div
 				className="flex flex-col items-center justify-center bg-contain cursor-pointer w-72 h-72 mobile:w-40 mobile:h-40 mr-3 ml-3 text-textColor rounded-2xl border border-solid border-box hover:border-details"
 				onClick={click}>
-				{img !== '' ? (
+				{img ? (
 					<img
 						className="w-full h-full bg-box rounded-t-xl"
 						src={img}
 						alt={title}
 					/>
 				) : (
-					<div className="flex items-center justify-center w-full h-full bg-box hover:text-textColor duration-300 hover:bg-details rounded-t-xl text-3xl mobile:text-xl font-semibold">
+					<div className="flex items-center text-center justify-center w-full h-full bg-box hover:text-textColor duration-300 hover:bg-details rounded-t-xl text-3xl mobile:text-xl font-semibold">
 						{title}
 					</div>
 				)}
--- src/components/RowBoxex/index.tsx
+++ src/components/RowBoxex/index.tsx
@@ -3,21 +3,21 @@ import { Link, useNavigate } from 'react-router-dom';
 import Title from '../../style/Title';
 import BoxButton from '../BoxButton';
 
-export interface ICategoryBox {
+export interface IRowBox {
 	NameCategory: string;
 	idCategory: string;
 	Product: Array<{ ProductId: string; image: string; NameProduct: string }>;
 }
 
-const CategoryBox = ({ NameCategory, idCategory, Product }: ICategoryBox) => {
+const RowBox = ({ NameCategory, idCategory, Product }: IRowBox) => {
 	const navigate = useNavigate();
 	return (
-		<div className="flex flex-col w-full mobile:h-60 mobile:overflow-y-hidden">
+		<div className="flex flex-col mobile:w-screen mobile:h-full mobile:overflow-y-hidden">
 			<Title>
 				<Link to={`/category/${idCategory}`}>{NameCategory}</Link>
 			</Title>
-			<div className="mobile:w-full mobile:h-full mobile:overflow-y-hidden">
-				<div className="mobile:flex mobile:flex-row mobile:w-max mobile:overflow-x-scroll mobile:h-full mt-2">
+			<div className="mobile:w-full mobile:h-full mobile:overflow-x-auto mobile:overflow-y-hidden">
+				<div className="mobile:flex mobile:flex-row mobile:w-max mobile:overflow-x-auto mobile:h-full mt-2">
 					{Product.map((el, i) => (
 						<BoxButton
 							key={i}
@@ -32,4 +32,4 @@ const CategoryBox = ({ NameCategory, idCategory, Product }: ICategoryBox) => {
 	);
 };
 
-export default CategoryBox;
+export default RowBox;
