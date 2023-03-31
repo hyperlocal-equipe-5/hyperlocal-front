e37850f43844323bd74a05e574eeb12e7e1adacf
TiagoBraga <ogait.desenvolvedor.jr@gmail.com>
2023-03-30 21:53:48 -0300

    feat: create box pedido cozinha #10

--- src/pages/Form/OrderForm/index.tsx
+++ src/pages/Form/OrderForm/index.tsx
@@ -11,13 +11,50 @@ const OrderForm = () => {
 		<div className={styled.box_pedido}>
 			<h2 className={styled.titulo}>Pedidos</h2>
 			<div className={styled.box_itens}>
-				<ul>
-					<li>Pedido nº: </li>
-					<li>Veg-Bruguer</li>
-					<li>Suco de Laranja</li>
-					<li>Pudim de leite</li>
-				</ul>
+				<h2>Nº: </h2>
+				<div>
+					<ul>
+						<li>VegBuguer</li>
+						<li>Coca</li>
+						<li>Sorvete</li>
+					</ul>
+				</div>
 			</div>
+
+			{/* <div className={styled.box_cozinha}>
+        <h2>Nº: </h2>
+        <h3>Hamburguer</h3>
+        <div>
+          <ul>
+            <li>pão</li>
+            <li>2 carnes</li>
+            <li>alface</li>
+            <li>cebola</li>
+            <li>mussarela</li>
+          </ul>
+        </div>
+        <h2>Extras</h2>
+        <div>
+          <ul>
+            <li>Bacon</li>
+            <li>Ovo</li>
+          </ul>
+        </div>
+        <h2>Bebidas</h2>
+        <div className={styled.box_bebidas}>
+          <ul>
+            <li>Coca</li>
+          </ul>
+        </div>
+        <h2>Sobremesas</h2>
+        <div className={styled.box_bebidas}>
+          <ul>
+            <li>Sorvete</li>
+          </ul>
+        </div>
+
+      </div> */}
+
 			<div className={styled.ordem_pedido}>
 				<h3>Retirada</h3>
 				<p>Seu pedido é o nº 2 da fila</p>
